0411bc7b14c6efb249c6afb5b2196318b62ba5e1
Nithin1908 <76480955+ngworks1909@users.noreply.github.com>
2023-12-17 20:03:29 +0530

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,8 +22,8 @@ export default function Main() {
                     <p className=" short-text">I'm an undergraduate student from Anurag University</p>
                     </div>
                     <div data-aos="fade-up" className="social-buttons">
-                        <a href="/" className="hire-me-button" >
-                             <span> Hire Me</span>
+                        <a href="https://ngworks-portfolio.netlify.app/static/media/Nithin%20Resume.6492b27d676c12ea34a8.pdf" target="_blank" className="hire-me-button" >
+                             <span> View Resume</span>
                         </a>
                         <a href={resumePDF} className="resume-button" download>
                             <div className="flex items-center gap-1">
